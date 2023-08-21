8a7022f7a0203d10ec2c8a067c1017d924e906af
obaidofj <obaidofj@gmail.com>
2023-08-21 13:56:21 +0300

    add port

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import dotenv from "dotenv";
 import bodyParser from 'body-parser';
 import { BookRoutes } from './bookRoutes.js';
 const app = express();
-const port = 3000;
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 // app.use(cors());
 app.get("/health", function (req, res) {
@@ -10,6 +12,6 @@ app.get("/health", function (req, res) {
 });
 // Initialize the book routes
 app.use('/api/books', BookRoutes);
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import dotenv from "dotenv";
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { BookRoutes } from './bookRoutes.js';
 
 const app = express();
-const port = 3000;
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 // app.use(cors());
@@ -14,7 +18,7 @@ app.get("/health", function (req, res) {
 // Initialize the book routes
 app.use('/api/books', BookRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
   
